890ddb952d419d9a4d8fb2ff5c23299ce100873b
Arinout <arant5@yandex.ru>
2022-04-22 21:50:42 +0500

    mesto v3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,27 +2,34 @@ const openPopupButton = document.querySelector ('.profile__edit');
 const popup = document.querySelector ('.popup');
 const popupCloseButton = popup.querySelector ('.popup__close-button');
 
-function popupOpenToggle(evt) {
-    popup.classList.toggle('popup__active');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('.popup__input_element_name');
+const professionInput = popup.querySelector('.popup__input_element_profession');
+
+const nameOutput = document.querySelector('.profile__name');
+const professionOutput = document.querySelector('.profile__profession');
+
+function popupOpenAdd(evt) {
+    popup.classList.add('popup_active');
+    nameInput.value = nameOutput.textContent;
+    professionInput.value = professionOutput.textContent
 }
 
-openPopupButton.addEventListener('click', popupOpenToggle);
-
-popupCloseButton.addEventListener('click', popupOpenToggle);
-
-const formElement = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('.popup__name');
-const jobInput = popup.querySelector('.popup__profession');
+function popupOpenRemove(evt) {
+    popup.classList.remove('popup_active');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     const nameInputValue = nameInput.value;
-    const jobInputValue = jobInput.value;
-    const nameOutput = document.querySelector('.profile__name');
-    const professionOutput = document.querySelector('.profile__profession');
+    const jobInputValue = professionInput.value;
     nameOutput.textContent = nameInputValue;
     professionOutput.textContent = jobInputValue;
-    popup.classList.toggle('popup__active');
+    popupOpenRemove();
 }
+
+openPopupButton.addEventListener('click', popupOpenAdd);
+
+popupCloseButton.addEventListener('click', popupOpenRemove);
 
 formElement.addEventListener('submit', formSubmitHandler); 
